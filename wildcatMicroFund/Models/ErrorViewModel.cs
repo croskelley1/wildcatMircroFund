d40c1f2b039de8b68064bcab0f48afd8c25d6e98
Jaden's X1 <Jaden's X1@DESKTOP-NAL10QE>
2022-11-03 12:34:34 -0600

    Add project files.

---
+++ wildcatMicroFund/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace wildcatMicroFund.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
