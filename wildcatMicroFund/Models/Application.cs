70bfddb3ea0a2e9e2f552f9809bb3f3544d3b74a
croskelley1 <croskelley1@mail.weber.edu>
2022-11-03 13:04:54 -0600

    Application model example added

---
+++ wildcatMicroFund/Models/Application.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace wildcatMicroFund.Models
+{
+    public class Application
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Company Name")]
+        public String? CompanyName { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        
+        [Display(Name = "App Status")]
+        public int AppStatus { get; set; }
+
+    }
+}
