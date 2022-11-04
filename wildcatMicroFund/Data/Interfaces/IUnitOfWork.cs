94341eae42b7a51cef001bb3f4f20bb04287e046
bshiple <bradenshipley@mail.weber.edu>
2022-11-04 15:50:14 -0600

    Added models to IUnitOfWork

--- wildcatMicroFund/Data/Interfaces/IUnitOfWork.cs
+++ wildcatMicroFund/Data/Interfaces/IUnitOfWork.cs
@@ -6,6 +6,27 @@ namespace wildcatMicroFund.Interfaces
     {
         //Data Accessors
         public IRepository<Application> Application { get; }
+        public IRepository<ApplicationStatus> ApplicationStatus { get; }
+        public IRepository<Availability> Availability { get; }
+        public IRepository<Award> Award { get; }
+        public IRepository<AwardType> AwardType { get; }
+        public IRepository<Document> Document { get; }
+        public IRepository<DocumentType> DocumentType { get; }
+        public IRepository<Note> Note { get; }
+        public IRepository<NoteType> NoteType { get; }
+        public IRepository<PitchEvent> PitchEvent { get; }
+        public IRepository<PitchEventApplication> PitchEventApplication { get; }
+        public IRepository<Question> Question { get; }
+        public IRepository<Response> Response { get; }
+        public IRepository<Role> Role { get; }
+        public IRepository<Score> Score { get; }
+        public IRepository<Status> Status { get; }
+        public IRepository<Survey> Survey { get; }
+        public IRepository<SurveyQuestion> SurveyQuestion { get; }
+        public IRepository<SurvType> SurveyQestion { get; }
+        public IRepository<User> User { get; }
+        public IRepository<UserAssignment> UserAssignment { get; }
+        public IRepository<UserRole> UserRole { get; }
 
         //save changes to data source
         void Commit();
