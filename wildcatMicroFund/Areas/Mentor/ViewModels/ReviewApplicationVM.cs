d98ee29483f5c669b18afcc189e0c0904b0ed916
C. Jaden Roskelley <76494708+croskelley1@users.noreply.github.com>
2022-11-19 17:51:43 -0700

    Mentor now can review a list of applications in the "Submitted" category, as well as view a single application and a list of new stati to change it to. Need to get the Update action updated to actually save the chagne.

--- wildcatMicroFund/Areas/Mentor/ViewModels/ReviewApplicationVM.cs
+++ wildcatMicroFund/Areas/Mentor/ViewModels/ReviewApplicationVM.cs
@@ -6,7 +6,8 @@ namespace wildcatMicroFund.Areas.Mentor.ViewModels
     public class ReviewApplicationVM
     {
         public ReviewApplication? ReviewApplication { get; set; }
-        public IEnumerable<SelectListItem>? ApplicationList { get; set; }
+        public Application? Application { get; set; }
+        public Status? Status { get; set; }
         public IEnumerable<SelectListItem>? StatusList { get; set; }
     }
 }
