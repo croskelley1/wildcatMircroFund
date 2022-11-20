f95a1a18431b07abcff94e5b0c3f2adf177df149
C. Jaden Roskelley <76494708+croskelley1@users.noreply.github.com>
2022-11-20 14:55:10 -0700

    Displaying question summaries and question details.

--- wildcatMicroFund/Areas/Judge/Controllers/PitchCriteria/PitchCriteriaController.cs
+++ wildcatMicroFund/Areas/Judge/Controllers/PitchCriteria/PitchCriteriaController.cs
@@ -1,12 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using wildcatMicroFund.Interfaces;
 using wildcatMicroFund.Models;
+using wildcatMicroFund.Areas.Judge.ViewModels;
+
 
 [Area("Judge")]
 public class PitchCriteriaController : Controller
 {
 
     private readonly IUnitOfWork _unitOfWork;
+    public PitchCriteriaVM PitchJudgeCriteriaList { get; set; }
 
     public PitchCriteriaController(IUnitOfWork unitOfWork)//Dependency Injection
     {
@@ -15,8 +18,30 @@ public class PitchCriteriaController : Controller
 
     public ViewResult PitchCriteria()
     {
-        IEnumerable<QuestionUse> PitchJudgeCriteriaList = _unitOfWork.QuestionUse.List(u => u.QCategory.QCategoryID == 4, u => u.QuestionUseID, "Question,QCategory"); //_unitOfWork is the database, Applications is the table, GetAll puts rows in a list
+        var qdList = _unitOfWork.QuestionDetail.List(null, null, "Question");
+        var questions = _unitOfWork.Question.List();
+        var QCategories = _unitOfWork.QCategory.List();
+        PitchJudgeCriteriaList = new PitchCriteriaVM
+        {
+
+            PitchCriteria = new QuestionUse(),
+            Question = _unitOfWork.Question.Get(a => a.Id == 2),
+            Category = _unitOfWork.QCategory.Get(c => c.QCategoryID == 4),
+            QuestionDetailList = _unitOfWork.QuestionDetail.List(null, null, "Question"),
+            QuestionList = _unitOfWork.Question.List(null, q => q.Id, null),
+            QCategoryList = QCategories.Select(c => new Microsoft.AspNetCore.Mvc.Rendering.SelectListItem { Value = c.QCategoryID.ToString(), Text = c.QCategoryDesc })
+
+
+            //        public QuestionUse? PitchCriteria { get; set; }
+            //public Question? Question { get; set; }
+            //public QCategory? Category { get; set; }
+
+            //public IEnumerable<SelectListItem> QuestionDetailList { get; set; }
+            //public IEnumerable<SelectListItem> QuestionList { get; set; }
+            //public IEnumerable<SelectListItem> QCategoryList { get; set; }
+        };
+
+        //IEnumerable<QuestionUse> PitchJudgeCriteriaList = _unitOfWork.QuestionUse.List(u => u.QCategory.QCategoryID == 4, u => u.QuestionUseID, "Question,QCategory"); //_unitOfWork is the database, Applications is the table, GetAll puts rows in a list
         return View(PitchJudgeCriteriaList);
     }
 }
-
--- wildcatMicroFund/Areas/Judge/ViewModels/PitchCriteriaVM.cs
+++ wildcatMicroFund/Areas/Judge/ViewModels/PitchCriteriaVM.cs
@@ -5,8 +5,13 @@ namespace wildcatMicroFund.Areas.Judge.ViewModels
 {
     public class PitchCriteriaVM
     {
+
         public QuestionUse? PitchCriteria { get; set; }
-        public IEnumerable<SelectListItem> QuestionList { get; set; }
-        public IEnumerable<SelectListItem> QCategory { get; set; }
+        public Question? Question { get; set; }
+        public QCategory? Category { get; set; }
+
+        public IEnumerable<QuestionDetail> QuestionDetailList { get; set; }
+        public IEnumerable<Question> QuestionList { get; set; }
+        public IEnumerable<SelectListItem> QCategoryList { get; set; }
     }
 }
