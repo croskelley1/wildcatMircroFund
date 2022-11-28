2ef5deb8c8a9552af39a8ee25f9defd5b92ef92a
MichaelTokarev <michael.voskanyan@icloud.com>
2022-11-27 19:06:21 -0700

    Added way to view previous applications and updated the HttpPost to update the Application and User tables including UserID, role, and app status

--- wildcatMicroFund/Areas/Entrepreneur/Controllers/NewApplication/NewApplicationController.cs
+++ wildcatMicroFund/Areas/Entrepreneur/Controllers/NewApplication/NewApplicationController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration.UserSecrets;
 using System.Diagnostics;
+using System.Security.Claims;
 using wildcatMicroFund.Interfaces;
 using wildcatMicroFund.Models;
 
@@ -16,9 +18,16 @@ public class NewApplicationController : Controller
 
     public ViewResult Index()
     {
-        return View();
+        var claimID = (ClaimsIdentity)User.Identity;
+        var claim = claimID.FindFirst(ClaimTypes.NameIdentifier);
+        
+        IEnumerable<UserAssignment> appList = _unitOfWork.UserAssignment.List(a => a.User.Id == claim.Value);
+        if (appList != null)
+            return View(appList);
+        else
+            return View();
     }
-
+    
     public ViewResult ApplicationForm()
     {
         return View();
@@ -31,7 +40,18 @@ public class NewApplicationController : Controller
 
         if (ModelState.IsValid)
         {
+            var claimID = (ClaimsIdentity)User.Identity;
+            var claim = claimID.FindFirst(ClaimTypes.NameIdentifier);
+
             _unitOfWork.Application.Add(obj); //internal add
+
+            var _UserAssignment = new UserAssignment();
+            _UserAssignment.Application = _unitOfWork.Application.Get(a => a.CompanyName == obj.CompanyName && a.CreatedDate == obj.CreatedDate);
+            _UserAssignment.User = _unitOfWork.ApplicationUser.Get(a => a.Id == claim.Value);
+            _UserAssignment.ApplicationAssignmentType = _unitOfWork.UserApplicationAssignmentType.GetById(4);
+
+            _unitOfWork.UserAssignment.Add(_UserAssignment); 
+
             _unitOfWork.Commit(); //physical commit to DB table
             TempData["success"] = "Application created Successfully";
             return RedirectToAction("Index");
